517f489dbe23b09e080c8277a0ca61d3549c582c
Vinayak Hejib <vinayak.hejib@hm.com>
2025-06-12 22:49:43 +0530

    Initial Commit

---
+++ DinaCharya/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DinaCharya
+//
+//  Created by Hejib Vinayak (BT: Product Dev. & Eng.: Product Engineering CoE) on 12/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DinaCharya/DinaCharyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinaCharyaApp.swift
+//  DinaCharya
+//
+//  Created by Hejib Vinayak (BT: Product Dev. & Eng.: Product Engineering CoE) on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DinaCharyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
